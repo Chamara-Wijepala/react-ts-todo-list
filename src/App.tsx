a82d8e3389aefe19d1ddf357577b096940f7a935
Chamara-Wijepala <mr.worf777@gmail.com>
2022-07-15 16:31:23 +0530

    Add themed background image and toggle theme icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import GlobalStyles from "./assets/Global.styled";
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 
-const background = require("./assets/images/bg-mobile-light.jpg");
+import { GlobalStyles, lightTheme, darkTheme } from "./assets/Global.styled";
+import bgDesktopLight from "./assets/images/bg-desktop-light.jpg";
+import bgDesktopDark from "./assets/images/bg-desktop-dark.jpg";
+import themeBtnLight from "./assets/icons/icon-sun.svg";
+import themeBtnDark from "./assets/icons/icon-moon.svg";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  function toggleTheme() {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  }
+
   return (
-    <>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <img src={background} alt="" />
-    </>
+      <img src={theme === "light" ? bgDesktopLight : bgDesktopDark} alt="" />
+      <div>
+        <header>
+          <h1>T O D O</h1>
+          <button type="button" onClick={toggleTheme}>
+            <img
+              src={theme === "light" ? themeBtnLight : themeBtnDark}
+              alt="Dark Mode"
+            />
+          </button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
