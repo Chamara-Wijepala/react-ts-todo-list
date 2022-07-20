b45c08b1b1261ae8997a78cfdf1d444845998233
Chamara-Wijepala <mr.worf777@gmail.com>
2022-07-20 15:28:25 +0530

    Add margin-left to DeleteButton

--- src/layouts/Main/styles/DeleteButton.styled.ts
+++ src/layouts/Main/styles/DeleteButton.styled.ts
@@ -4,6 +4,7 @@ import StyledButton from "../../../assets/styles/Button.styled";
 
 const StyledDeleteButton = styled(StyledButton)`
   color: ${({ theme }) => theme.text_main};
+  margin-left: auto;
 `;
 
 export default StyledDeleteButton;
